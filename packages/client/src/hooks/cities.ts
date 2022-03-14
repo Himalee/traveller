0ed6462acf7347a1b69b3e8b5ba90170668d5ee8
Himalee <himalee.tailor@gmail.com>
2022-03-14 15:13:46 +0000

    Fix linting errors

--- packages/client/src/hooks/cities.ts
+++ packages/client/src/hooks/cities.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@apollo/client'
-import type { CitiesRequestVars, CitiesResponseData} from '../queries';
+import type { CitiesRequestVars, CitiesResponseData } from '../queries'
 import { GET_CITIES } from '../queries'
 
 export function useFilteredCities(filter: 'wishlist' | 'visited', value: boolean) {
